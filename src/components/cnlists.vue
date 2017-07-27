800acf6e44d1f16f705211cee99589662e54224b
csuhan <1593407377@qq.com>
2017-07-27 22:21:46 +0800

    fix cnlists

--- src/components/cnlists.vue
+++ src/components/cnlists.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-for="list in cnlists">
-            <router-link v-bind:to="{name:'DetailPage',params:{id:list.id}}" tag="div">
+            <router-link v-bind:to="{name:'DetailPage',params:{id:list.id,title:list.title,dept:list.dept,pubtime:list.pubtime,viewer:list.viewer}}" tag="div">
                 <div class="weui-panel">
                     <div class="weui-panel__bd">
                         <div class="weui-media-box weui-media-box_text">
